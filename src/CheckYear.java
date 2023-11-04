ef5a8990be0676cfcb36e6c2d298bffc8c931a55
Valery <valeryroj@yandex.ru>
2023-11-04 16:32:24 +0300

    разделил на методы и дописал комментарии

---
+++ src/CheckYear.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CheckYear {
+    static int checkYear() throws IllegalArgumentException {
+        System.out.print("Год начала жизни на проценты: ");
+        Scanner scanner = new Scanner(System.in);
+        int startYear = scanner.nextInt();
+
+        if (startYear < 2002 || startYear > 2021)
+            throw new IllegalArgumentException("Год начала жизни должен быть от 2002 до 2021");
+        return startYear;
+    }
+}
